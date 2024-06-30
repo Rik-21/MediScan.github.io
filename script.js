a2eef933c8e0e08a815206bdad75c4fdc5c59b37
Rupantar Mondal <rupantarkirnahar@gamil.com>
2024-06-30 07:04:25 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const dropArea = document.getElementById("drop-area");
+const inputFile = document.getElementById("input-file");
+const imageView = document.getElementById("img-view");
+
+inputFile.addEventListener("change", uploadImage);
+
+function uploadImage(){
+    let imgLink = URL.createObjectURL(inputFile.files[0]);
+    imageView.style.backgroundImage = `url(${imgLink})`;
+    imageView.textContent = "";
+    imageView.style.border = 0;
+}
+
+dropArea.addEventListener("dragover", function(e){
+    e.preventDefault();
+});
+dropArea.addEventListener("drop", function(e){
+    e.preventDefault();
+    inputFile.files = e.dataTransfer.files;
+    uploadImage();
+});
+
+
+// ====================================    sticky navbar   ==========================================
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+// Get the navbar
+var navbar = document.querySelector('.navbar');
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
